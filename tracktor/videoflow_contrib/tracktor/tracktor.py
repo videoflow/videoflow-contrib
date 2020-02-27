e36e965d39a627b3f1d80e7c9072d0e8cdcf1010
Jadiel deArmas <jadielam@gmail.com>
2020-02-27 17:49:11 -0500

    asdf

--- tracktor/videoflow_contrib/tracktor/tracktor.py
+++ tracktor/videoflow_contrib/tracktor/tracktor.py
@@ -84,7 +84,6 @@ class TracktorFromFrames(OneTaskProcessorNode):
         t_frame.unsqueeze_(0)
         self._tracker.step({'img': t_frame})
         results = self._tracker.get_current_tracks()
-        print(type(results))
         return results
     
 
