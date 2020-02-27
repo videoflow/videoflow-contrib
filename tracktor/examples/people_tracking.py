fba35c19fbf30c57a1365de0d64e468ee7e9702b
Jadiel deArmas <jadielam@gmail.com>
2020-02-27 18:06:28 -0500

    asdf

--- tracktor/examples/people_tracking.py
+++ tracktor/examples/people_tracking.py
@@ -66,7 +66,7 @@ def main():
             index, next_frame = reader.next()
         except:
             break
-        print(next_frames.shape)
+        print(next_frame.shape)
         tracks = track_from_frames.process(next_frame)
         transformed_tracks = tracks_to_annotator.process(tracks)
         annotated_frame = annotator.process(next_frame, tracks)
