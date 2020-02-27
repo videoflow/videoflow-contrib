d502ef8462b4c4e8c7bd3553e11527932eda500d
Jadiel deArmas <jadielam@gmail.com>
2020-02-27 16:15:16 -0500

    Did small change to url of video

--- tracktor/videoflow_contrib/tracktor/tracktor.py
+++ tracktor/videoflow_contrib/tracktor/tracktor.py
@@ -19,10 +19,10 @@ class TracktorFromFrames(OneTaskProcessorNode):
     https://github.com/phil-bergmann/tracking_wo_bnw
     '''
 
-    def __init__(self, interpolate = False):
+    def __init__(self, device_type = 'gpu', interpolate = False):
         self._tracker = None
         self._interpolate = interpolate
-        super(TracktorFromFrames, self).__init__()
+        super(TracktorFromFrames, self).__init__(device_type = device_type)
     
     def open(self):
         #1. Load detection model
